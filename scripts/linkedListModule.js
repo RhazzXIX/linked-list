1afc645f395643814378aa1142bc89ef79749000
RhazzXIX <jm21tonio@gmail.com>
2023-02-15 16:54:34 +0800

    Add insertAt and removeAt methods
    Created insertAt method to add a node at a specific index.
    Created removeAt method to remove the node at the specified index only.

--- scripts/linkedListModule.js
+++ scripts/linkedListModule.js
@@ -1,30 +1,3 @@
-["log", "warn", "error"].forEach((methodName) => {
-  const originalMethod = console[methodName];
-  console[methodName] = (...args) => {
-    let initiator = "unknown place";
-    try {
-      throw new Error();
-    } catch (e) {
-      if (typeof e.stack === "string") {
-        let isFirst = true;
-        for (const line of e.stack.split("\n")) {
-          const matches = line.match(/^\s+at\s+(.*)/);
-          if (matches) {
-            if (!isFirst) {
-              // first line - current function
-              // second line - caller (what we are looking for)
-              initiator = matches[1];
-              break;
-            }
-            isFirst = false;
-          }
-        }
-      }
-    }
-    originalMethod.apply(console, [...args, "\n", `  at ${initiator}`]);
-  };
-});
-
 const LinkedList = (function () {
   let List = null;
 
@@ -109,7 +82,6 @@ const LinkedList = (function () {
 
   const find = function (data) {
     let index = 0;
-    console.log(data);
     if (List === null) {
       console.log("The List is null");
       return;
@@ -140,6 +112,41 @@ const LinkedList = (function () {
     return string;
   };
 
+  const insertAt = function (data, index) {
+    if (index === 0) {
+      prepend(data);
+      console.log("Made the data as the head node");
+      return;
+    }
+    if (index >= size() + 1) {
+      append(data);
+      console.log("Inserted at the tail node");
+      return;
+    }
+    const indexOfPrevNode = index - 1;
+    const oldNode = at(index);
+    const prevNode = at(indexOfPrevNode);
+    const newNode = Node(data, oldNode);
+    prevNode.next = newNode;
+  };
+
+  const removeAt = function (index) {
+    if (index >= size()) {
+      pop();
+      console.log("Deleted the tail node");
+      return;
+    }
+    if (index === 0) {
+      List = List.next;
+      console.log("Deleted the head Node");
+      return;
+    }
+    const currentNode = at(index);
+    const prevNodeIndex = index - 1;
+    const prevNode = at(prevNodeIndex);
+    prevNode.next = currentNode.next;
+  };
+
   return {
     append,
     prepend,
@@ -151,6 +158,8 @@ const LinkedList = (function () {
     contains,
     find,
     toString,
+    insertAt,
+    removeAt,
   };
 })();
 
@@ -161,29 +170,31 @@ list1.prepend(testData);
 
 list1.prepend(165465465);
 
-// console.log(list1.size());
+console.log(list1.size());
 
-// console.log(list1.tail());
+console.log(list1.tail());
 
 list1.append("testing append");
-// console.log(list1.tail());
+console.log(list1.tail());
 
-// console.log(list1.head());
-// console.log(list1.size());
+console.log(list1.head());
 
 const obj1 = {
   prop: "test",
 };
 
-// console.log(list1.tail());
-// console.log(list1.contains(testData));
-// console.log(list1.contains('lkadfka'));
-// console.log(list1.contains(165465465));
-// console.log(list1.find('lkadfka'));
-// console.log(list1.find(165465465));
+console.log(list1.contains(testData));
+console.log(list1.contains("lkadfka"));
+console.log(list1.contains(165465465));
+console.log(list1.find("lkadfka"));
+console.log(list1.find(165465465));
 
-list1.append(obj1);
-
-console.log(list1.find(obj1));
 console.log(typeof list1.at(3));
+list1.insertAt(obj1, 4);
+console.log(list1.find(obj1));
+list1.insertAt("insert", 6);
+
+console.log(list1.toString());
+console.log(list1.size());
+list1.removeAt(1);
 console.log(list1.toString());
