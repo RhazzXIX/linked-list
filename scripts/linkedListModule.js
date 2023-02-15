0db9573a6042f672f81b6dd7e7a891e54df1ea83
RhazzXIX <jm21tonio@gmail.com>
2023-02-15 12:35:12 +0800

    Create intitial Linked-list data structure
    Created a Linked-list factory that will create a linked-list data
    structure.
    Created a Node factory that will create an object with data and a link
    to the next object.
    Created a linked-list method prepend that will add a data at the start
    of the list.
    Created a linked-list method head() that will return the head of the
    list.

--- scripts/linkedListModule.js
+++ scripts/linkedListModule.js
@@ -1,1 +1,57 @@
-console.error('Hello Up-skilling World!');
+
+['log', 'warn', 'error'].forEach((methodName) => {
+  const originalMethod = console[methodName];
+  console[methodName] = (...args) => {
+    let initiator = 'unknown place';
+    try {
+      throw new Error();
+    } catch (e) {
+      if (typeof e.stack === 'string') {
+        let isFirst = true;
+        for (const line of e.stack.split('\n')) {
+          const matches = line.match(/^\s+at\s+(.*)/);
+          if (matches) {
+            if (!isFirst) { // first line - current function
+                            // second line - caller (what we are looking for)
+              initiator = matches[1];
+              break;
+            }
+            isFirst = false;
+          }
+        }
+      }
+    }
+    originalMethod.apply(console, [...args, '\n', `  at ${initiator}`]);
+  };
+});
+
+const LinkedList = (function () {
+  let node = null;
+
+  const Node = function (data, next) {
+    return { data, next };
+  };
+
+  const prepend = function (item) {
+    node = Node(item, node);
+  };
+
+  const head = function () {
+    return node.data;
+  };
+
+  console.log(node);
+
+  return { prepend, head };
+})();
+
+const list1 = LinkedList;
+const testData = "This is test Data";
+
+list1.prepend(testData);
+
+console.log(list1.head());
+
+list1.prepend(165465465);
+
+console.log(list1.head());
