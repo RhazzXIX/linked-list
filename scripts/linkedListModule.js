08d31f4ecbbfbb31ff3747a2930f910778e1511a
RhazzXIX <jm21tonio@gmail.com>
2023-02-15 14:29:59 +0800

    Add 'tail', 'append', 'size', & 'at' methods
    Created 'tail' method to show the last node of the linked-list data
    structure.
    Created 'append' method to add a data at the end of the linked-list data
    structure.
    Created 'size' method to show the size of the data structure.
    Created 'at' method to show the node at the specific data structure
    index.

--- scripts/linkedListModule.js
+++ scripts/linkedListModule.js
@@ -1,18 +1,18 @@
-
-['log', 'warn', 'error'].forEach((methodName) => {
+["log", "warn", "error"].forEach((methodName) => {
   const originalMethod = console[methodName];
   console[methodName] = (...args) => {
-    let initiator = 'unknown place';
+    let initiator = "unknown place";
     try {
       throw new Error();
     } catch (e) {
-      if (typeof e.stack === 'string') {
+      if (typeof e.stack === "string") {
         let isFirst = true;
-        for (const line of e.stack.split('\n')) {
+        for (const line of e.stack.split("\n")) {
           const matches = line.match(/^\s+at\s+(.*)/);
           if (matches) {
-            if (!isFirst) { // first line - current function
-                            // second line - caller (what we are looking for)
+            if (!isFirst) {
+              // first line - current function
+              // second line - caller (what we are looking for)
               initiator = matches[1];
               break;
             }
@@ -21,28 +21,66 @@
         }
       }
     }
-    originalMethod.apply(console, [...args, '\n', `  at ${initiator}`]);
+    originalMethod.apply(console, [...args, "\n", `  at ${initiator}`]);
   };
 });
 
 const LinkedList = (function () {
-  let node = null;
+  let List = null;
 
   const Node = function (data, next) {
     return { data, next };
   };
 
-  const prepend = function (item) {
-    node = Node(item, node);
+  const prepend = function (data) {
+    const newHead = Node(data, List);
+    List = newHead;
   };
 
   const head = function () {
-    return node.data;
+    return List;
   };
 
-  console.log(node);
+  const tail = function (data) {
+    if (List === null) return List;
+    let tempNode = List;
+    while (tempNode.next !== null) {
+      tempNode = tempNode.next;
+    }
+    return tempNode;
+  };
 
-  return { prepend, head };
+  const append = function (data) {
+    const nodeTail = tail();
+    const newNode = Node(data, null)
+    nodeTail.next = newNode;
+  }
+
+  const size = function () {
+    let length = 0;
+    if (List === null) return length;
+    let tempNode = List;
+    while (tempNode !== null) {
+      tempNode = tempNode.next;
+      length += 1;
+    }
+    return length;
+  };
+
+  const at = function (num) {
+    let index = 0;
+    let tempNode = List;
+    for (index; index < num; index += 1) {
+      if (tempNode === null) {
+        const errorMessage = `Index is up to ${size()} only`
+        throw new Error(errorMessage);
+      };
+      tempNode = tempNode.next;
+    }
+    return tempNode
+  }
+
+  return { append, prepend, head, tail, size, at };
 })();
 
 const list1 = LinkedList;
@@ -54,4 +92,11 @@ console.log(list1.head());
 
 list1.prepend(165465465);
 
-console.log(list1.head());
+console.log(list1.size());
+
+console.log(list1.tail());
+
+list1.append('testing append');
+console.log(list1.tail());
+
+console.log(list1.at(0));
